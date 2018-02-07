5eed5ae521a4765d8b15fbc5c9c8f31b4782abbd
fede.franchi99 <fede.franchi99@gmail.com>
2018-02-07 12:18:28 -0500

    commit2

--- es1gioconumero.php
+++ es1gioconumero.php
@@ -1,19 +1,37 @@
 <!DOCTYPE hmtl>
+
+    <?php
+    if(!isset($_POST["numerodaindovinare"])) //verifica se la 
+    {
+      $count= 0;
+     // $messaggicontatore="prova numero "+$count;
+      $numerodaindovinare = rand(1,100);
+     //$_POST["numerodaindovinare"];
+     // $_POST["numerodaindovinare"]= rand(1,100);
+      //$_POST["count"]++;
+      $messaggio="regole del gioco<br> il giocatore deve indovinare un numero compreso tra 1 e 100 <br> con max 7 tentativi<br> ";  
+    }
+       else
+       {
+        // if($_POST["numerogiocato"]==$_POST["numerodaaindovinare"]) //verifico se il numero scritto dall'utente coincide con quello da indovinare
+         //{
+           
+         //}
+       }
+     //potrebbe essere necessario modificare il numero
+       
+    ?>
 <html>
   <head>
-    <form action="" method="post">
-    <title>Gioconumero</title>
-  </head>
+   <title>Gioconumero</title>
+  </head> 
   <body>
     <h1>Gioco dell'indovina il numero</h1>
-    <?php 
-    $count=0; //potrebbe essere necessario modificare il numero
-    echo "regole del gioco<br>";
-    echo "il giocatore deve indovinare un numero compreso tra 1 e 100<br>";
-    echo "con max 7 tentativi<br>";
-    ?>
+    <form action="" method="post">
+    <p><?php echo $messaggio ?> </p>
     <br>
-    <button type="submit" id="bottone">Inizia il gioco</button>
+    <input type="hidden" name="numerodaindovinare" value="<?php echo $numerodaindovinare ?>">
+    <button type="submit" id="bottone">ciao</button>
   </form>
   </body>
 </html>
