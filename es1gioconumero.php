adeca1ef009c85b63278584ba5bd39b952c24867
fede.franchi99 <fede.franchi99@gmail.com>
2018-02-14 05:13:46 -0500

    eserciziounoterminato

--- es1gioconumero.php
+++ es1gioconumero.php
@@ -1,6 +1,10 @@
 <!DOCTYPE hmtl>
 
     <?php
+if($_POST["bottone"]=="rigioca")
+{
+  $_POST["numerodaindovinare"]=null;
+}
     if(!isset($_POST["numerodaindovinare"])) //verifica se la 
     {
       $count= 0;
@@ -10,14 +14,49 @@
      // $_POST["numerodaindovinare"]= rand(1,100);
       //$_POST["count"]++;
       $messaggio="regole del gioco<br> il giocatore deve indovinare un numero compreso tra 1 e 100 <br> con max 7 tentativi<br> ";  
+      $messaggiobottone="inizia il gioco";
+      $tipo="hidden";
+     // $tipoindizio="hidden";
     }
-       else
+       else if($_POST["numerodaindovinare"]!=$_POST["numeroinserito"] and $_POST["contatore"]<7)
        {
+         $numerodaindovinare=$_POST["numerodaindovinare"];
+         $count=$_POST["contatore"]+1;
+         $messaggio="tentativo numero $count <br>inserisci un numero";
+         $tipo="text";
+         $messaggiobottone="conferma";
+        // $tipoindizio="hidden";
+         if($_POST["contatore"]>=1)    //if(isset($_POST["numeroinserito"]))
+         {
+          // $tipoindizio="text";
+           if($_POST["numerodaindovinare"]>$_POST["numeroinserito"])
+           {
+             $indicazione="il numero inserito e' troppo piccolo";
+           }
+           else
+           {
+             $indicazione="il numero inserito e' troppo grande";
+           }
+         }
         // if($_POST["numerogiocato"]==$_POST["numerodaaindovinare"]) //verifico se il numero scritto dall'utente coincide con quello da indovinare
          //{
            
          //}
        }
+else if($_POST["numerodaindovinare"]==$_POST["numeroinserito"]) //qui
+{
+  $indicazione="BRAVO!";
+  $count=$_POST["contatore"];
+  $messaggio="HAI INDOVINATO IN $count tentativi";
+  $messaggiobottone="rigioca";
+}
+else if($_POST["contatore"]=7) //non funziona qualcosa
+{
+  $indicazione="SPIACENTE...";
+  $count=$_POST["contatore"];
+  $messaggio="NON HAI INDOVINATO IL NUMERO";
+  $messaggiobottone="rigioca";
+}
      //potrebbe essere necessario modificare il numero
        
     ?>
@@ -28,10 +67,14 @@
   <body>
     <h1>Gioco dell'indovina il numero</h1>
     <form action="" method="post">
-    <p><?php echo $messaggio ?> </p>
+      <p><?php if(isset($indicazione)){echo "$indicazione";}?></p>
+    <p><?php echo "$messaggio"?> </p>  
     <br>
-    <input type="hidden" name="numerodaindovinare" value="<?php echo $numerodaindovinare ?>">
-    <button type="submit" id="bottone">ciao</button>
+      <input type="hidden"  value="<?php echo "$count" ?>" name="contatore" >
+    <input type="hidden" name="numerodaindovinare" value="<?php echo "$numerodaindovinare" ?>">
+      <input type="<?php echo "$tipo" ?>" name="numeroinserito" >
+      <br><br>
+    <button type="submit" name="bottone" value="<?php echo "$messaggiobottone"?>"><?php echo "$messaggiobottone"?></button>
   </form>
   </body>
 </html>
