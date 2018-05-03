0297333dd46819aed33313943066915f417433ad
fede.franchi99 <fede.franchi99@gmail.com>
2018-05-03 04:47:16 -0400

    combo

--- pagmeteoj.js
+++ pagmeteoj.js
@@ -1,8 +1,7 @@
 //alert("ciao");
-function gettempbycity($city)
+function gettempbycity(city)
 {
-
-  var urlcit="http://api.openweathermap.org/data/2.5/weather?q="+$city+"&APPID=4af89dd6ce566691af4c7f4ed65ec3c5&units=metric";
+  var urlcit="http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=4af89dd6ce566691af4c7f4ed65ec3c5&units=metric";
   var weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
   const monthnames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
   "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
@@ -13,7 +12,7 @@ function gettempbycity($city)
     var day=weekday[dt.getDay()];
     var month=monthnames[dt.getMonth()];
     var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-    var info=$('<a class"info">'+day+' '+month+' '+dt.getDate()+' '+dt.getFullYear()+' '+time+' GMT'+dt.getTimezoneOffset()+'</a>');
+    var info=$('<a class="info">'+day+' '+month+' '+dt.getDate()+' '+dt.getFullYear()+' '+time+' GMT'+dt.getTimezoneOffset()+'</a>');
     var table=$('<table class="table"><tbody></tbody></table>');
      var righe=$('<tr><td><h4>'+result.name+','+result.sys.country+'(lon='+result.coord.lon+', lat='+result.coord.lat+')</h4></td></tr><tr><td>main</td><td>'+result.weather[0].main+'</td></tr> <tr><td>description</td><td>'+result.weather[0].description+'</td></tr> <tr><td>temperature</td><td>'+result.main.temp+'&deg;C</td></tr> <tr><td>pressure</td><td>'+result.main.pressure+'hpa</td></tr> <tr><td>humidity</td><td>'+result.main.humidity+'%</td></tr> <tr><td>wind speed</td><td>'+result.wind.speed+'m/s</td></tr> <tr><td>wind deg</td><td>'+result.wind.deg+'</td></tr> <tr><td>visibility</td><td>'+result.visibility+'</td></tr>');
    
@@ -25,17 +24,33 @@ function gettempbycity($city)
   });
   
 }
-function gettempbycityforecast($city2)
+function gettempbycityforecast(city)
 {
-  
+  //alert(city);
+  var urlcit="http://api.openweathermap.org/data/2.5/forecast?q="+city+"&APPID=4af89dd6ce566691af4c7f4ed65ec3c5&units=metric";
+  $.getJSON(urlcit,function(resultt)
+   {
+    //alert(result);
+    if(resultt)
+      {
+        //alert("informazioni ricevute");
+        var combotime=$('<select id="combo"></select>');
+        $.each(resultt.list,function(k,v){
+          //alert(v.dt_txt);
+          combotime.append($('<option value="'+k+'">'+v['dt_txt']+'</option>'));
+        });
+        $("#tab2").append(combotime);
+      }
+    
+  });
 }
 $(document).ready(function(){
 
   $("#okbutt").click(function(){
-  gettempbycity($("#nomecitt").val());
-  $("#okbutt2").click(function(){
-  gettempbycityforecast($("#nomecitt2").val());
-  })
+  gettempbycity($("#nomecitt").val())
+  });
+   $("#okbuttt").click(function(){
+  gettempbycityforecast($("#nomecittt").val())
   });
 });
 //function funzselezionecitt()
